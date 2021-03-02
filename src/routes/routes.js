cae600c6be4bbe2e21169bc93422b1e9e6976ad6
Vitor Carlos <vitorcarlos1312@gmail.com>
2021-03-02 20:05:22 -0300

    create aula 5

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,5 +3,7 @@ const ClienteController = require("../controllers/clienteController.js");
 const router = express.Router();
 
 router.post("/cliente", ClienteController.Insert);
+router.get("/cliente", ClienteController.SelectAll);
+router.get("/cliente/:id", ClienteController.SelectDetail);
 
 module.exports = router;
