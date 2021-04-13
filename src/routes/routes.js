c9967fa6302e5d9ec3f6e2b8f09bcef16fdb2fe0
Vitor Carlos <vitorcarlos1312@gmail.com>
2021-04-13 20:29:47 -0300

    Resolve bugs

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const ClienteController = require("../controllers/clienteController.js");
 const router = express.Router();
 
-router.post("/cliente", ClienteController.Insert);
+router.post("/CriarCliente", ClienteController.Insert);
 router.get("/cliente", ClienteController.SelectAll);
 router.get("/cliente/:id", ClienteController.SelectDetail);
 router.put("/cliente/:id", ClienteController.Update);
